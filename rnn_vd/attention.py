77752e8e43b0e9718874e3838c43cfbc24d1d491
Yermek Kapushev <kapushev@gmail.com>
2020-04-04 10:53:55 +0300

    added TransformerEncoderLayer with optional `kdim` and `vdim` parameters

--- rnn_vd/attention.py
+++ rnn_vd/attention.py
@@ -97,3 +97,61 @@ class GeneralDotProductScore(DotProductScore):
         return score
 
 
+class TransformerEncoderLayer(torch.nn.Module):
+    r"""This is a fork of the `torch.nn.TransformerEncoderLayer` with
+    additional parameters `kdim` and `vdim`.
+    """
+
+    def __init__(self, d_model, nhead, kdim=None, vdim=None,
+                 dim_feedforward=2048, dropout=0.1, activation="relu",
+                 linear=torch.nn.Linear):
+        super().__init__()
+        self.self_attn = torch.nn.MultiheadAttention(
+            d_model, nhead, kdim=kdim, vdim=vdim, dropout=dropout)
+        
+        # Implementation of Feedforward model
+        self.linear1 = linear(d_model, dim_feedforward)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.linear2 = linear(dim_feedforward, d_model)
+
+        self.norm1 = torch.nn.LayerNorm(d_model)
+        self.norm2 = torch.nn.LayerNorm(d_model)
+        self.dropout1 = torch.nn.Dropout(dropout)
+        self.dropout2 = torch.nn.Dropout(dropout)
+
+        self.activation = _get_activation_fn(activation)
+
+    def __setstate__(self, state):
+        if 'activation' not in state:
+            state['activation'] = F.relu
+        super(TransformerEncoderLayer, self).__setstate__(state)
+
+    def forward(self, src, src_mask=None, src_key_padding_mask=None):
+        # type: (Tensor, Optional[Tensor], Optional[Tensor]) -> Tensor
+        r"""Pass the input through the encoder layer.
+
+        Args:
+            src: the sequence to the encoder layer (required).
+            src_mask: the mask for the src sequence (optional).
+            src_key_padding_mask: the mask for the src keys per batch (optional).
+
+        Shape:
+            see the docs in Transformer class.
+        """
+        src2 = self.self_attn(src, src, src, attn_mask=src_mask,
+                              key_padding_mask=src_key_padding_mask)[0]
+        src = src + self.dropout1(src2)
+        src = self.norm1(src)
+        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
+        src = src + self.dropout2(src2)
+        src = self.norm2(src)
+        return src
+
+
+def _get_activation_fn(activation):
+    if activation == "relu":
+        return F.relu
+    elif activation == "gelu":
+        return F.gelu
+
+    raise RuntimeError("activation should be relu/gelu, not {}".format(activation))
